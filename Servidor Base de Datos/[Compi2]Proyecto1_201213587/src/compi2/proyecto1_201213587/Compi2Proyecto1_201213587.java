69f32b1e40dc3af214b1d325ea19b8958c8d00fb
Titus <titus.pivaral@gmail.com>
2017-08-13 22:02:40 -0600

    Creando Gramatica USQL

--- Servidor Base de Datos/[Compi2]Proyecto1_201213587/src/compi2/proyecto1_201213587/Compi2Proyecto1_201213587.java
+++ Servidor Base de Datos/[Compi2]Proyecto1_201213587/src/compi2/proyecto1_201213587/Compi2Proyecto1_201213587.java
@@ -5,6 +5,11 @@
  */
 package compi2.proyecto1_201213587;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author Titus
@@ -15,7 +20,31 @@ public class Compi2Proyecto1_201213587 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        try {
+            Runtime runtime = Runtime.getRuntime();
+            String[] cmd = new String[4];
+            cmd[0] = "cmd.exe";
+            cmd[1] = "/C";
+            cmd[2] = "javacc";
+            cmd[3] = "D:\\Titus\\Documents\\Proyectos\\Compiladores 2\\Proyecto 1\\FISQL\\Servidor Base de Datos\\[Compi2]Proyecto1_201213587\\src\\Analisis\\usql_grammar.jj";
+            /*
+                        para poder ejecutar esta clase es necesario setear JavaCC a las varibles de entorno del sistema 
+                        de tal forma que el simbolo del sistema (la terminal) reconozca el comando javacc o jjtree
+             */
+            Process proc = runtime.exec(cmd);
+            InputStream is = proc.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.print(e.toString());
+        }
+        
+        Consola a = new Consola();
+        a.setVisible(true);
+    }    
 }
